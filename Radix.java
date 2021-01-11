0262160fe4c9376275b47ec8f6648c101f2ac7c4
tomas-20 <tacuna20@stuy.edu>
2021-01-11 13:16:28 -0500

    added the length function

--- Radix.java
+++ Radix.java
@@ -1,5 +1,9 @@
 public class Radix {
+  private static double log10 = Math.log(10);
   public static int nth(int n, int col) {
     return n / (int) Math.pow(10, col) % 10;
+  }
+  public static int length(int n) {
+    return (int) (Math.log(n) / log10);
   }
 }
